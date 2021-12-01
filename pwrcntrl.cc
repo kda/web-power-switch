9bb1546ac2514cabb8cfdce8958e6a28afdcd48b
Kevin Athey <kda@pobox.com>
2021-11-30 20:41:02 -0800

    enable full command line

--- pwrcntrl.cc
+++ pwrcntrl.cc
@@ -7,43 +7,39 @@
 
 
 int main(int iArgc, char* szArgv[]) {
-  cxxopts::Options options(szArgv[0], "control web power switches");
+  cxxopts::Options options(szArgv[0], "pwrcntrl: control web power switches");
   options
     .positional_help("(all|switch_name|outlet_name) ([show]|on|off|toggle|cycle)")
     .show_positional_help();
   options
     .allow_unrecognised_options()
     .add_options()
-      ("a,all", "show all outlets (even if something other than 'show' is specified).", cxxopts::value<bool>())
       ("credentials", "provide pairs of username:password to use on switch(es).", cxxopts::value<std::vector<std::string>>())
-      ("command", "[show]|on|off|toggle|cycle: show, turn on, turn off, toggle or cycle outlet.", cxxopts::value<std::vector<std::string>>())
-      ("d,discover", "if switch name is not known, then attempt to discover it")
-      ("h,host", "name of host (supports IP, as well)")
+      ("command", "[show]|on|off|toggle|cycle: show, turn on, turn off, toggle or cycle outlet.", cxxopts::value<std::string>())
       ("help", "show help")
-      ("o,outlet", "name of outlet")
-      ("r,reset", "even if switch locations are known, go find them again")
-      ("s,switch", "name of switch")
+      ("r,reset", "even if switch locations are known, go find them again", cxxopts::value<bool>()->default_value("false"))
+      ("t,target", "'all', name of switch, name of outlet", cxxopts::value<std::string>())
       ("v,verbose", "increate verbosity of output")
     ;
 
-  options.parse_positional({"command"});
+  options.parse_positional({"target", "command"});
   auto optionsResult = options.parse(iArgc, szArgv);
   //std::cout << "parsed" << std::endl;
 
-  if (optionsResult.count("help") ||
-      (optionsResult.count("command") &&
-       optionsResult["command"].as<std::vector<std::string>>()[0] == "help")) {
+  std::string target;
+  if (optionsResult.count("target")) {
+    target = optionsResult["target"].as<std::string>();
+  } else {
+    std::cout << options.help() << std::endl;
+    return 0;
+  }
+
+  if (optionsResult.count("help")) {
     std::cout << options.help() << std::endl;
     return 0;
   }
   //std::cout << "no help" << std::endl;
 
-  //cxxopts::value<std::string>()->default_value("show");
-  std::string command = {"unknown"};
-  if (optionsResult.count("command") != 0) {
-    command = optionsResult["command"].as<std::vector<std::string>>()[0];
-  }
-  //std::cout << "command: " << command << std::endl;
 
   std::unique_ptr<WebPowerSwitchManager> wpsm(new WebPowerSwitchManager);
 
@@ -75,16 +71,12 @@ int main(int iArgc, char* szArgv[]) {
     wpsm->resetCache();
   }
 
-  //std::cout << "target: -" << target << "-" << std::endl;
-
   // If 'all', then only implement show.
-  if (optionsResult.count("all")) {
-    if (optionsResult["all"].as<bool>()) {
-      wpsm->dumpSwitches(std::cout);
-      return 0;
-    }
+  if (target == "all") {
+    wpsm->dumpSwitches(std::cout);
+    return 0;
   }
-#ifdef kda_COMMENTED_OUT
+
   auto wps = wpsm->getSwitch(target);
   if (wps != nullptr) {
     wps->dumpOutlets(std::cout);
@@ -99,6 +91,14 @@ int main(int iArgc, char* szArgv[]) {
 
   std::cout << wps->name() << ": " << *(wps->getOutlet(target)) << std::endl;
 
+  std::string command;
+  if (optionsResult.count("command")) {
+    command = optionsResult["command"].as<std::string>();
+  } else {
+    // no command
+    return 0;
+  }
+
   if (strncasecmp(command.c_str(), "on", 2) == 0) {
     wps->on(target);
   } else if (strncasecmp(command.c_str(), "off", 3) == 0) {
@@ -111,10 +111,10 @@ int main(int iArgc, char* szArgv[]) {
   } else if (strncasecmp(command.c_str(), "toggle", 5) == 0) {
     wps->toggle(target);
   } else {
+    std::cout << "ERROR: unrecognized command: " << command << std::endl;
     return 0;
   }
   std::cout << wps->name() << ": " << *(wps->getOutlet(target)) << std::endl;
-#endif /* kda_COMMENTED_OUT */
 
   return 0;
 }
